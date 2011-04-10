db7aa3c82b94496afc535adc39e4da1262624ab3
Grégory PAUL <paulgreg@gmail.com>
2011-04-10 12:26:11 +0200

    Add some controls

--- controller.py
+++ controller.py
@@ -10,19 +10,28 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+def goToHome(self, error = None):
+  lasts = urlController.get_lasts()
+  self.response.headers['Content-Type'] = 'text/html'
+  path = os.path.join(os.path.dirname(__file__), 'home.html')
+  self.response.out.write(template.render(path, { 'lasts': lasts, 'error': error } ))
+
 class MainPage(webapp.RequestHandler):
   def get(self): 
-    self.goToHome()
-
-  def goToHome(self):
-    lasts = urlController.get_lasts()
-    self.response.headers['Content-Type'] = 'text/html'
-    path = os.path.join(os.path.dirname(__file__), 'home.html')
-    self.response.out.write(template.render(path, { 'lasts': lasts } ))
+    goToHome(self)
 
 class SavePage(webapp.RequestHandler):
   def post(self):
-    shortened_url = urlController.save(self.request.get('url'))
+    url = self.request.get('url')
+    if not url or len(url) == 0: 
+      goToHome(self, 'Please type an URL.')
+      return
+
+    if not url.startswith('http://') and not url.startswith('https://'):
+      goToHome(self, 'URL should start with "http://" or "https://".')
+      return
+
+    shortened_url = urlController.save(url)
     self.redirect('/')
 
 class RedirectPage(webapp.RequestHandler):
