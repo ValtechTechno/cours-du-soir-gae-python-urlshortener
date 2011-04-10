a0a0fd6a316781eb33e1b0c64b7dddaa11b6d5b6
Grégory PAUL <paulgreg@gmail.com>
2011-04-10 15:08:19 +0200

    Add a tool to copy url in clipboard

--- scripts/home.js
+++ scripts/home.js
@@ -1,10 +1,20 @@
 (function() {
    if (!window.addEventListener) return;
 	window.addEventListener("load", function() {
+
+		ZeroClipboard.setMoviePath('scripts/ZeroClipboard.swf');
+
 		if (!document.querySelectorAll) return;
 		var links = document.querySelectorAll('a.shorturl');
 		for(var i = 0; i < links.length; i++) {
+			// Add host to link
 			links[i].innerHTML = window.location.protocol + '//' + window.location.host + links[i].innerHTML;
+
+			// Add code to copy to clipboard
+			var clip = new ZeroClipboard.Client();
+         clip.setText(links[i].innerHTML);
+			clip.glue(links[i].nextSibling);
 		}
+
 	}, false)
 })();
