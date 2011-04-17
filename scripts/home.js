ad154aa2b308575d97f4cba29a0736d25bfc14c5
Grégory PAUL <paulgreg@gmail.com>
2011-04-17 18:38:21 +0200

    Remove Zeroclipboard wich doesn't work on iOs and add code to simply select link

--- scripts/home.js
+++ scripts/home.js
@@ -1,21 +1,39 @@
 (function() {
+
    if (!window.addEventListener) return;
 	window.addEventListener("load", function() {
 
-		ZeroClipboard.setMoviePath('scripts/ZeroClipboard.swf');
-
 		if (!document.querySelectorAll) return;
+
+		var notification = document.getElementById('notification');
 
 		var links = document.querySelectorAll('.shorturl');
 		for(var i = 0; i < links.length; i++) {
-			// Add host to link
-			links[i].innerHTML = window.location.protocol + '//' + window.location.host + links[i].innerHTML;
+			(function() {
+				var input = links[i];
+				input.value = window.location.protocol + '//' + window.location.host + input.value;
 
-			// Add code to copy to clipboard
-			var clip = new ZeroClipboard.Client();
-         clip.setText(links[i].innerHTML);
-			clip.glue(links[i].nextSibling);
+				input.nextSibling.addEventListener("click", function(evt) { 
+					input.select();
+					notification.style.top = '-40px';
+					showSlowly();
+					window.setTimeout(hideSlowly, 4000);
+				}, false);
+			})();
 		}
+
+		var showSlowly = function() {
+			notification.style.top = (parseInt(notification.style.top, 10) + 5) + 'px';
+			if (parseInt(notification.style.top, 10) < 0)
+				setTimeout(showSlowly, 10);
+		};
+
+
+		var hideSlowly = function() {
+			notification.style.top = (parseInt(notification.style.top, 10) - 5) + 'px';
+			if (parseInt(notification.style.top, 10) > -40)
+				setTimeout(hideSlowly, 10);
+		};
 
 	}, false)
 })();
